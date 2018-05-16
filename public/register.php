b2356af781a4da00f93d7e63f95b765e45fec675
furusawaAsobimo <38462152+furusawaAsobimo@users.noreply.github.com>
2018-05-16 19:49:39 +0900

    register api test5 commit

--- public/register.php
+++ public/register.php
@@ -4,25 +4,15 @@ require_once '../vendor/autoload.php';
 
 $groupsApi = (new \MailerLiteApi\MailerLite(API_KEY))->groups();
 
-//$subscriber = [
-//    'email' => $_GET['email'],
-//    'fields' => [
-//        'name' => $_GET['name'],
-//        'last_name' => $_GET['last_name'],
-//        'company' => $_GET['company'],
-//    ]
-//];
-
 $subscriber = [
-    'email' => 'john@example.com',
+    'email' => $_GET['email'],
     'fields' => [
-        'name' => 'John',
-        'last_name' => 'Doe',
-        'company' => 'John Doe Co.'
+        'name' => $_GET['name'],
+        'last_name' => $_GET['last_name'],
+        'company' => $_GET['company'],
     ]
 ];
 
 $response = $groupsApi->addSubscriber(GROUP_ID, $subscriber);
-
 var_dump($response);
 exit;
